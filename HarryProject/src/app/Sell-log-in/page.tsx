1c82469ac8d78f73d13e534848f4b4371215c588
Harshada Mali <harshadamali2003@gmail.com>
2025-08-20 16:10:15 +0530

    Fix: Sell-log-in

--- HarryProject/src/app/Sell-log-in/page.tsx
+++ HarryProject/src/app/Sell-log-in/page.tsx
@@ -4,7 +4,10 @@ import LoginForm from "../components/LoginForm";
 export default function SellLoginPage() {
   return (
     <div>
-      <LoginForm />
+      <LoginForm 
+      onClose={() => console.log("Login form closed")}
+        onSwitch={() => console.log("Switch to signup")}
+      />
     </div>
   );
 }
