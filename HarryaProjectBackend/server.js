5bca0f3ad29b48234de1f6068ac6c19fb1f4169a
Harshada Mali <harshadamali2003@gmail.com>
2025-08-20 19:22:33 +0530

    remove app.js

--- HarryaProjectBackend/server.js
+++ HarryaProjectBackend/server.js
@@ -43,5 +43,6 @@ app.use("/uploads", express.static("uploads"));
 
 
 // Start Server
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+// const PORT = process.env.PORT || 5000
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+export default app
