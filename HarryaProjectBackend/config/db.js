8b4ce690a07c60a67906cc19b6798c667d09fcd5
Harshada Mali <harshadamali2003@gmail.com>
2025-08-31 23:19:39 +0530

    fix .env

--- HarryaProjectBackend/config/db.js
+++ HarryaProjectBackend/config/db.js
@@ -1,13 +1,18 @@
-import mongoose from "mongoose"
+// config/db.js
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI,)
-    console.log("MongoDB connected successfully")
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // wait 10s before timing out
+    });
+    console.log("MongoDB connected successfully");
   } catch (error) {
-    console.error("MongoDB connection failed:", error)
-    process.exit(1)
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
   }
-}
+};
 
-export default connectDB
+export default connectDB;
