b3a6bf800682cc54c1d076f33eb246cc438c4dd4
Harshada Mali <harshadamali2003@gmail.com>
2025-09-01 14:50:05 +0530

    change db.js

--- HarryaProjectBackend/config/db.js
+++ HarryaProjectBackend/config/db.js
@@ -4,9 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 10000, // wait 10s before timing out
+      serverSelectionTimeoutMS: 10000, // optional: wait 10s before timing out
     });
     console.log("MongoDB connected successfully");
   } catch (error) {
